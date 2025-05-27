5c590cdaef878718f26733be39bc0ccfaa8a5a69
MozzammelRidoy <mozzammelridoy5iii@gmail.com>
2025-05-27 22:01:32 +0600

    customer inpurt validation zod schema added

---
+++ src/app/modules/customer/customer_validationZodSchema.ts
@@ -0,0 +1,47 @@
+import { z } from "zod";
+
+const createCustomerValidationSchema = z.object({
+  body: z.object({
+    name: z
+      .string({
+        required_error: "Name is required",
+      })
+      .min(3, {
+        message: "Name must be at least 3 characters long",
+      })
+      .max(70, {
+        message: "Name must not exceed 70 characters",
+      })
+      .regex(/^[A-Za-z\s]+$/, {
+        message: "Name must contain only letters",
+      }),
+    email: z
+      .string({
+        required_error: "Email is required",
+      })
+      .min(3, {
+        message: "Email must be at least 3 characters long",
+      })
+      .max(70, {
+        message: "Email must not exceed 70 characters",
+      })
+      .email({
+        message: "Invalid email address",
+      }),
+
+    phone: z
+      .string({
+        required_error: "Phone number is required",
+      })
+      .length(11, {
+        message: "Phone number must be exactly 11 characters long",
+      })
+      .regex(/^[0-9]+$/, {
+        message: "Phone number must contain only numbers",
+      }),
+  }),
+});
+
+export const CustomerValidation = {
+  createCustomerValidationSchema,
+};
