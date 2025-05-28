98aa24e65eddebe4a2def470b9aa9fae554dfe67
MozzammelRidoy <mozzammelridoy5iii@gmail.com>
2025-05-28 22:19:08 +0600

    bike crated business logic applyed

--- src/app/modules/bikes/bikes_service.ts
+++ src/app/modules/bikes/bikes_service.ts
@@ -1,8 +1,19 @@
 import { Bike } from "../../../../generated/prisma";
+import AppError from "../../errors/AppError";
+import prisma from "../../shared/prisma";
 
 // create a bike into db.
 const createBikeIntoDB = async (payload: Bike) => {
-  console.log(payload);
+  const customer = await prisma.customer.findUnique({
+    where: { customerId: payload.customerId },
+  });
+  if (!customer) {
+    throw new AppError(404, "customerId", "Customer id not found");
+  }
+  const bike = await prisma.bike.create({
+    data: payload,
+  });
+  return bike;
 };
 
 export const BikesServices = {
